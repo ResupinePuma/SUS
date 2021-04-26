ba91d6b8e8d2fe0e7e7443a227ca258159f57692
ResupinePuma <cherchip50@gmail.com>
2021-04-26 22:00:25 +0300

    added tests

---
+++ sus/tests/test_engines.py
@@ -0,0 +1,23 @@
+from sus.engines import telegram, reddit, rss
+import vcr
+import unittest
+
+
+class EngineTests(unittest.TestCase):
+    @vcr.use_cassette('./sus/tests/cassettes/telegram.yaml')
+    def test_telegram(self):
+        self.assertEqual(len(telegram.scrab(
+            {"url": "https://t.me/s/durov", "time_limit_hours": 24*31*6})), 19)
+
+    @vcr.use_cassette('./sus/tests/cassettes/reddit.yaml')
+    def test_reddit(self):
+        self.assertEqual(len(reddit.scrab(
+            {"url": "https://www.reddit.com/r/announcements/", "time_limit_hours": 24*31})), 1)
+
+    @vcr.use_cassette('./sus/tests/cassettes/rss.yaml')
+    def test_rss(self):
+        self.assertEqual(len(rss.scrab(
+            {"url": "https://feeds.bbci.co.uk/news/world/europe/rss.xml"})), 8)
+
+
+            
