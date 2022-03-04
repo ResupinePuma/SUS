0d5a7c141210f924c5d8682daf1c4390eeb599c9
ResupinePuma <34252154+ResupinePuma@users.noreply.github.com>
2022-03-04 20:08:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='sus',
     version='0.1a',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
     entry_points={
     'console_scripts':
         ['sus = sus:main']
